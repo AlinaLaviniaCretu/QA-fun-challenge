75266f6fa3f30c3addbbdca4dada78889def54e1
Cretu Alina-Lavinia <cra2rt@bosch.com>
2022-05-29 13:04:32 +0200

    add QA automation project files.

---
+++ README.md
@@ -0,0 +1,47 @@
+# QA automation challange
+
+This mini-project contains the following files:
+
+- **my_program.py** 
+  
+  Contains the python methods used for:
+  
+     - Opening and parsing the input file
+     - Extracting the data from the XML: the GCP ids, GCP coordinates, GCP ids that can be found in images
+     - Computing the area covered by the GCP points
+     - Creating a file (if it does not exist) and writing the number of GCP points, the area covered by them and for each of the GCPs in how many images it can be seen.
+
+
+- **example.p4d**
+  
+  - Contains the input data structured in an XML fashion
+
+
+- **output.txt**
+  
+  - Is the file produced after running the program and contains the requested information: number of GCP points, the area covered by them and for each of the GCPs in how many images it can be seen.
+## Prerequisites
+- Python version equal or grater than 3.8
+
+
+## Installation
+
+1. Create a python virtual environment.
+2. Change the work dir to the root directory of the venv.
+3. Use the package manager [pip](https://pip.pypa.io/en/stable/) to install _shapely_ package.
+
+```bash
+pip install shapely
+```
+
+
+## Usage
+1. Change the work dir to the root directory of the virtual environment.
+2. Use the following command to run the program:
+
+```bash
+python my_program.py example.p4d output.txt
+```
+
+## Assumptions
+- Input data is structured in an XML fashion
---
+++ my_program.py
@@ -0,0 +1,92 @@
+import xml.etree.ElementTree as ET
+from shapely.geometry import Polygon
+import sys
+
+
+class GCP:
+    def __init__(self):
+        self.input_file = self.get_input_file()
+        self.output_file = self.get_output_file()
+        self.xml_object = self.__open_and_parse_input_file()
+
+    @staticmethod
+    def get_input_file() -> str:
+        """Take the input file from terminal."""
+        input_file = sys.argv[1]
+        return input_file
+
+    @staticmethod
+    def get_output_file() -> str:
+        """Take the output file from terminal."""
+        output_file = sys.argv[2]
+        return output_file
+
+    def __open_and_parse_input_file(self):
+        """Parse XML input document into element tree and return its root element"""
+        return ET.parse(self.input_file).getroot()
+
+    def get_xml_info(self, tag: str, prop: str) -> list:
+        """Find all matching subelements by tag name in document order.
+        Return a list containing their attribute values.
+
+        *tag* is a string having an element tag
+        *prop* the attribute of the tag
+        """
+        info_list = list()
+        for type_tag in self.xml_object.findall("inputs/{}".format(tag)):
+            value = type_tag.get(prop)
+            info_list.append(value)
+        return info_list
+
+    def get_gcp_ids(self) -> list:
+        """Get all the GCP ids"""
+        return self.get_xml_info("gcps/GCP", "id")
+
+    def get_coordinates(self) -> list:
+        """Get the x and y coordinates of all GCP points"""
+        return [
+            [float(type_tag.get("x")), float(type_tag.get("y"))]
+            for type_tag in self.xml_object.findall("inputs/gcps/GCP")
+        ]
+
+    @staticmethod
+    def compute_area(data: list) -> float:
+        """Compute a crude approximation of the area covered by the GCPs"""
+        xs = [x[0] for x in data]
+        ys = [x[1] for x in data]
+        min(xs), max(xs), min(ys), max(ys)
+        rectangle_coords = [
+            (min(xs), min(ys)),
+            (max(xs), min(ys)),
+            (max(xs), max(ys)),
+            (min(xs), max(ys)),
+        ]
+        return Polygon(rectangle_coords).area
+
+    def get_gcp_from_images(self) -> list:
+        """Get all the GCPs ids that appear in images."""
+        return self.get_xml_info("images/image/imageGCP", "id")
+
+    def create_and_fill_output(self, gcps: list, area: float, gcp_in_images: list):
+        """Create the output file containing
+             - the number of GCPs,
+             - the area covered by them
+             - for each of the GCPs in how many images it can be seen
+        """
+        with open(self.output_file, "w") as f:
+            f.write(f"{str(len(gcps))}\n")
+            f.write(f"{area}\n")
+            for id in gcps:
+                f.write(f"{id}: {gcp_in_images.count(id)}\n")
+
+
+def main():
+    gcp_object = GCP()
+    gcp_ids = gcp_object.get_gcp_ids()
+    area = gcp_object.compute_area(gcp_object.get_coordinates())
+    gcps_from_img = gcp_object.get_gcp_from_images()
+    gcp_object.create_and_fill_output(gcp_ids, area, gcps_from_img)
+
+
+if __name__ == "__main__":
+    main()
